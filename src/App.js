74ce6f3f71265e44158e425ebda24d6a1e108807
BrainJarProductions <vgrilli1@gmail.com>
2020-04-21 21:01:07 -0400

    add shoppage, collection preview and item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { HomePage } from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 import './pages/homepage/homepage.scss';
 import './App.css';
 
@@ -15,7 +16,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
